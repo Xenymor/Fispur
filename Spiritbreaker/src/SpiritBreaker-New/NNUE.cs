49b922e6347349105669ba6fdcce6cf6baae25ce
Xenymor <101249409+Xenymor@users.noreply.github.com>
2026-08-30 18:23:08 +0200

    Create NNUE

---
+++ Spiritbreaker/src/SpiritBreaker-New/NNUE.cs
@@ -0,0 +1,79 @@
+using Spiritbreaker.API;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Spiritbreaker
+{
+    public static class NNUE
+    {
+        const int INPUT = 768;
+        const int HL = 128;              
+        const int QA = 255;
+        const int QB = 64;
+        const int QAB = QA * QB;          
+        const int SCALE = 400;
+
+        static readonly short[] l0w = new short[INPUT * HL];  
+        static readonly short[] l0b = new short[HL];
+        static readonly short[] l1w = new short[2 * HL];
+        static readonly short l1b;
+
+        static NNUE()
+        {
+            Stream? s = Assembly.GetExecutingAssembly().GetManifestResourceStream("net.bin");
+
+            using var r = new BinaryReader(s);
+            for (int i = 0; i < l0w.Length; i++) l0w[i] = r.ReadInt16();
+            for (int i = 0; i < l0b.Length; i++) l0b[i] = r.ReadInt16();
+            for (int i = 0; i < l1w.Length; i++) l1w[i] = r.ReadInt16();
+            l1b = r.ReadInt16();
+        }
+
+        static int SCReLU(int x)
+        {
+            int c = x < 0 ? 0 : x > QA ? QA : x;
+            return c * c;
+        }
+
+        public static int Evaluate(Board board)
+        {
+            Span<int> accW = stackalloc int[HL];
+            Span<int> accB = stackalloc int[HL];
+            for (int h = 0; h < HL; h++) { accW[h] = l0b[h]; accB[h] = l0b[h]; }
+
+            for (int colour = 0; colour <= 1; colour++)   // 0 = Weiss, 1 = Schwarz
+            {
+                bool white = colour == 0;
+                for (PieceType type = PieceType.Pawn; type <= PieceType.King; type++)
+                {
+                    int pc = (int)type - 1;                // Pawn..King -> 0..5
+                    ulong bb = board.GetPieceBitboard(type, white);
+                    while (bb != 0)
+                    {
+                        int sq = BitboardHelper.ClearAndGetIndexOfLSB(ref bb);   // a1=0..h8=63
+                        int wBase = (384 * colour + 64 * pc + sq) * HL;
+                        int bBase = (384 * (1 - colour) + 64 * pc + (sq ^ 56)) * HL;
+                        for (int h = 0; h < HL; h++)
+                        {
+                            accW[h] += l0w[wBase + h];
+                            accB[h] += l0w[bBase + h];
+                        }
+                    }
+                }
+            }
+
+            Span<int> boys = board.IsWhiteToMove ? accW : accB;
+            Span<int> opps = board.IsWhiteToMove ? accB : accW;
+
+            long sum = 0;
+            for (int h = 0; h < HL; h++)
+            {
+                sum += (long)SCReLU(boys[h]) * l1w[h];
+                sum += (long)SCReLU(opps[h]) * l1w[HL + h];
+            }
+
+            return (int)((sum / QA + l1b) * SCALE / QAB);
+        }
+    }
+}
--- Spiritbreaker/src/SpiritBreaker-New/SpiritBreaker.cs
+++ Spiritbreaker/src/SpiritBreaker-New/SpiritBreaker.cs
@@ -5,151 +5,138 @@ using System.Data;
 using System.Linq;
 using System.Numerics;
 
-public class SpiritBreaker : IChessBot
+namespace Spiritbreaker
 {
-    public string GetAuthor()
+    public class SpiritBreaker : IChessBot
     {
-        return "Xenymor";
-    }
 
-    public string GetName()
-    {
-        return "Spiritbreaker 0.3.0";
-    }
-
-
-    Move bestMove;
-    Dictionary<ulong, (int score, int alpha, int beta, int depthLeft, Move move)> transpositionTable = new Dictionary<ulong, (int score, int alpha, int beta, int depthLeft, Move move)>();
-
-    public (Move move, int eval) Think(Board board, Timer timer)
-    {
-        Move[] moves = board.GetLegalMoves();
-        bestMove = moves.Length == 0 ? Move.NullMove : moves[0];
-
-        long time = timer.MillisecondsRemaining / 20 + timer.IncrementMilliseconds / 2;
-        int depth = 1;
-        int eval = 0;
-
-        while (timer.MillisecondsElapsedThisTurn < time / 2)
+        public string GetAuthor()
         {
-            eval = AlphaBeta(board, 0, depth, -10_000_00, 10_000_00);
-            Console.WriteLine("info currmove " + Spiritbreaker.Chess.MoveUtility.GetMoveNameUCI(bestMove.move) + " depth " + depth + " score cp " + eval + " time " + timer.MillisecondsElapsedThisTurn);
-            depth++;
+            return "Xenymor";
         }
 
-        return (bestMove, eval);
-    }
-
-    private int AlphaBeta(Board board, int ply, int depthLeft, int alpha, int beta)
-    {
-        if (board.IsInCheckmate())
+        public string GetName()
         {
-            return -1000_00 + ply;
+            return "Spiritbreaker 0.3.0";
         }
 
-        if (board.IsDraw())
-        {
-            return 0;
-        }
 
-        Move[] moves;
-        bool qSearch = false;
-        int eval;
-        bool inCheck = board.IsInCheck();
+        Move bestMove;
+        Dictionary<ulong, (int score, int alpha, int beta, int depthLeft, Move move)> transpositionTable = new Dictionary<ulong, (int score, int alpha, int beta, int depthLeft, Move move)>();
 
-        if (depthLeft <= 0)
+        public (Move move, int eval) Think(Board board, Timer timer)
         {
-            qSearch = true;
-            if (!inCheck)
+            Move[] moves = board.GetLegalMoves();
+            bestMove = moves.Length == 0 ? Move.NullMove : moves[0];
+
+            long time = timer.MillisecondsRemaining / 20 + timer.IncrementMilliseconds / 2;
+            int depth = 1;
+            int eval = 0;
+
+            while (timer.MillisecondsElapsedThisTurn < time / 2)
             {
-                eval = Eval(board);
-                if (eval >= beta)
+                eval = AlphaBeta(board, 0, depth, -10_000_00, 10_000_00);
+                Console.WriteLine("info currmove " + Chess.MoveUtility.GetMoveNameUCI(bestMove.move) + " depth " + depth + " score cp " + eval + " time " + timer.MillisecondsElapsedThisTurn);
+                depth++;
+            }
+
+            return (bestMove, eval);
+        }
+
+        private int AlphaBeta(Board board, int ply, int depthLeft, int alpha, int beta)
+        {
+            if (board.IsInCheckmate())
+            {
+                return -1000_00 + ply;
+            }
+
+            if (board.IsDraw())
+            {
+                return 0;
+            }
+
+            Move[] moves;
+            bool qSearch = false;
+            int eval;
+            bool inCheck = board.IsInCheck();
+
+            if (depthLeft <= 0)
+            {
+                qSearch = true;
+                if (!inCheck)
                 {
-                    return eval;
+                    eval = NNUE.Evaluate(board);
+                    if (eval >= beta)
+                    {
+                        return eval;
+                    }
+                    if (eval > alpha)
+                    {
+                        alpha = eval;
+                    }
                 }
-                if (eval > alpha)
+            }
+
+            (int score, int alpha, int beta, int depthLeft, Move move) entry;
+            bool hasEntry = transpositionTable.TryGetValue(board.ZobristKey, out entry);
+
+            if (
+                hasEntry && ply != 0 && depthLeft <= entry.depthLeft
+                && (entry.score >= entry.beta && entry.score >= beta
+                    || entry.score > entry.alpha && entry.score < entry.beta
+                    || entry.score < entry.alpha && entry.score < alpha)
+                )
+            {
+                return entry.score;
+            }
+
+            moves = board.GetLegalMoves(qSearch && !inCheck);
+
+            moves = moves.OrderByDescending(move => hasEntry && entry.move.Equals(move) ? 10_000_000 : move.IsCapture ? (int)move.CapturePieceType * 1_000 - (int)move.MovePieceType : move.MovePieceType == PieceType.King ? 0 : (int)move.MovePieceType).ToArray();
+            Move bestMove = Move.NullMove;
+            int bestScore = -10_000_00;
+
+            foreach (Move move in moves)
+            {
+                board.MakeMove(move);
+                int score = -AlphaBeta(board, ply + 1, depthLeft - 1, -beta, -alpha);
+                board.UndoMove(move);
+
+                if (score >= beta)
                 {
-                    alpha = eval;
+                    transpositionTable[board.ZobristKey] = (score, alpha, beta, depthLeft, move);
+                    return score;
+                }
+                bestScore = Math.Max(score, bestScore);
+                if (score > alpha)
+                {
+                    bestMove = move;
+                    alpha = score;
                 }
             }
-        }
 
-        (int score, int alpha, int beta, int depthLeft, Move move) entry;
-        bool hasEntry = transpositionTable.TryGetValue(board.ZobristKey, out entry);
-
-        if (
-            hasEntry && ply != 0 && depthLeft <= entry.depthLeft 
-            && (entry.score >= entry.beta && entry.score >= beta 
-                || entry.score > entry.alpha && entry.score < entry.beta 
-                || entry.score < entry.alpha && entry.score < alpha)
-            )
-        {
-            return entry.score;
-        }
-
-        moves = board.GetLegalMoves(qSearch && !inCheck);
-
-        moves = moves.OrderByDescending(move => hasEntry && entry.move.Equals(move) ? 10_000_000 : move.IsCapture ? (int)move.CapturePieceType * 1_000 - (int)move.MovePieceType : move.MovePieceType == PieceType.King ? 0 : (int)move.MovePieceType).ToArray();
-        Move bestMove = Move.NullMove;
-        int bestScore = -10_000_00;
-
-        foreach (Move move in moves)
-        {
-            board.MakeMove(move);
-            int score = -AlphaBeta(board, ply + 1, depthLeft - 1, -beta, -alpha);
-            board.UndoMove(move);
-
-            if (score >= beta)
+            if (ply == 0)
             {
-                transpositionTable[board.ZobristKey] = (score, alpha, beta, depthLeft, move);
-                return score;
+                this.bestMove = bestMove;
             }
-            bestScore = Math.Max(score, bestScore);
-            if (score > alpha)
-            {
-                bestMove = move;
-                alpha = score;
-            }
-        }
 
-        if (ply == 0)
-        {
-            this.bestMove = bestMove;
-        }
-
-        if (!qSearch)
-        {
-            if (hasEntry)
+            if (!qSearch)
             {
-                if (depthLeft > entry.depthLeft)
+                if (hasEntry)
+                {
+                    if (depthLeft > entry.depthLeft)
+                    {
+                        transpositionTable[board.ZobristKey] = (bestScore, alpha, beta, depthLeft, bestMove);
+                    }
+                }
+                else
                 {
                     transpositionTable[board.ZobristKey] = (bestScore, alpha, beta, depthLeft, bestMove);
                 }
-            } else
-            {
-                transpositionTable[board.ZobristKey] = (bestScore, alpha, beta, depthLeft, bestMove);
+
             }
-           
+
+            return alpha;
         }
-
-        return alpha;
     }
-
-    int[] pieceVal = { 1_00, 3_00, 3_50, 5_00, 9_00, 100_00 };
-    private int Eval(Board board)
-    {
-        int score = 0;
-        for (int c = 0; c <= 1; c++) {
-            bool isWhite = board.IsWhiteToMove ? c == 0 : c == 1;
-            for (PieceType type = PieceType.Pawn; type <= PieceType.King; type++) {
-                ulong bitBoard = board.GetPieceBitboard(type, isWhite);
-                score += BitOperations.PopCount(bitBoard) * pieceVal[(int)type - 1];
-            }
-            score = -score;
-        }
-
-        return score;
-    }
-
-    
 }
