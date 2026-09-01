b9b304a5f3340a2d7efd2bef7168bc9433ba0b49
Xenymor <101249409+Xenymor@users.noreply.github.com>
2026-09-01 12:13:32 +0200

    Save SpiritBreaker 0.5.0
    Add history heuristic

--- Spiritbreaker-cli/Program.cs
+++ Spiritbreaker-cli/Program.cs
@@ -9,7 +9,7 @@ internal class Program
     {
         private static void Main(string[] args)
         {
-            IChessBot spiritBreaker = new Spiritbreaker.SpiritBreaker0_4_0.SpiritBreaker0_4_0();
+            IChessBot spiritBreaker = new Spiritbreaker.SpiritBreaker0_5_0.SpiritBreaker0_5_0();
             Type botType = spiritBreaker.GetType();
             Spiritbreaker.Chess.Board tempBoard = new Spiritbreaker.Chess.Board();
             tempBoard.LoadStartPosition();
