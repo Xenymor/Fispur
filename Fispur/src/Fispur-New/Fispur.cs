90ee5f3ff26e243e50199a9416ebfd620f5f6b21
Xenymor <101249409+Xenymor@users.noreply.github.com>
2026-09-23 00:02:29 +0200

    Remove Heap-Allocation
    bench 515733 nodes at 1009262 nps

--- Fispur/src/Fispur-New/Fispur.cs
+++ Fispur/src/Fispur-New/Fispur.cs
@@ -233,6 +233,7 @@ namespace FispurEngine
             return (bestMove, eval);
         }
 
+        [SkipLocalsInit]
         private int AlphaBeta(Board board, int ply, int depthLeft, int alpha, int beta, bool canNull = true)
         {
             if (stopSearch)
@@ -252,7 +253,6 @@ namespace FispurEngine
             }
 
             int ogAlpha = alpha;
-            Move[] moves;
             bool inCheck = board.IsInCheck();
             if (inCheck)
             {
@@ -329,7 +329,8 @@ namespace FispurEngine
 
             Move ttMove = hasEntry ? entry.move : Move.NullMove;
 
-            moves = board.GetLegalMoves(qSearch && !inCheck);
+            Span<Move> moves = stackalloc Move[218];
+            board.GetLegalMovesNonAlloc(ref moves, qSearch && !inCheck);
 
             if (moves.Length == 0)
                 return inCheck ? -MATE + ply
