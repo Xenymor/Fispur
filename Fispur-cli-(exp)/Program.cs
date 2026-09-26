3ca4d59d37feac607bd3f0a4047325a619eb8c77
Xenymor <101249409+Xenymor@users.noreply.github.com>
2026-09-26 14:55:34 +0200

    Fix Bounds
    bench: nodes=2236723 nps=2418078

--- Fispur-cli-(exp)/Program.cs
+++ Fispur-cli-(exp)/Program.cs
@@ -265,7 +265,7 @@ internal class Program
         new("MinIIRDepth",          0,      8,      () => Fispur.MinIIRDepth,           v => Fispur.MinIIRDepth = v,            true),
         new("CorrHistGrain",        0,      500,    () => Fispur.CORR_GRAIN,            v => Fispur.CORR_GRAIN = v),
         new("CorrHistScale",        0,      500,    () => Fispur.CORR_SCALE,            v => Fispur.CORR_SCALE = v),
-        new("CorrHistMax",          0,      500,    () => Fispur.CORR_MAX,              v => Fispur.CORR_MAX = v),
+        new("CorrHistMax",          0,      50000,  () => Fispur.CORR_MAX,              v => Fispur.CORR_MAX = v),
     };
 
     static void PrintSpsaInputs()
